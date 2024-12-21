ebc968d728ab3b37291d30d48904a00cc1cc94b1
Swarnava-paul <swarnavapaul108@gmail.com>
2024-12-21 17:46:46 +0530

    project setup successfull with typescript and server setup with Node

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import {Text } from '@chakra-ui/react'
+
+function App() {
+
+
+  return (
+    <>
+    <Text>hello</Text>
+    </>
+  )
+}
+
+export default App
